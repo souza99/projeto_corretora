530e90185dd5274b2a36b3f8fd2bb6668c56be5f
jpfsouza99 <jpfsouza99@gmail.com>
2022-11-01 13:32:07 -0300

    Espaço para tickets em destaque

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -2,82 +2,58 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:projeto_corretora/constants.dart';
 
+import 'header_with_search_box.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     //pega tamanho total da altura e largura da tela
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: <Widget>[
-        Container(
-          height: size.height * 0.2,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.2 - 27,
-                decoration: BoxDecoration(
-                  color: kPrimaryColor,
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(36),
-                    bottomRight: Radius.circular(36),
-                  ),
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      'Flz Colecionador!',
-                      style: Theme.of(context).textTheme.headline5?.copyWith(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                    Spacer(),
-                    Image.asset("assets\icons\logo.svg")
-                  ],
-                ),
-              ),
-              Positioned(
-                bottom: 0,
-                left: 0,
-                right: 0,
-                child: Container(
-                  alignment: Alignment.center,
-                  margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                  height: 54,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        offset: Offset(0, 10),
-                        blurRadius: 50,
-                        color: kPrimaryColor.withOpacity(0.23),
-                      ),
-                    ],
-                  ),
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          onChanged: (value) {},
-                          decoration: InputDecoration(
-                            hintText: "Pesquise aqui",
-                            hintStyle: TextStyle(
-                              color: kPrimaryColor.withOpacity(0.5),
-                            ),
-                            enabledBorder: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      SvgPicture.asset("assets\icons\search.svg"),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+    //scroll da tela
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithCustomeUnderline(text: "Em destaque")
+        ],
+      ),
+    );
+  }
+}
+
+class TitleWithCustomeUnderline extends StatelessWidget {
+  const TitleWithCustomeUnderline({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 24,
+      child: Stack(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: kDefaultPadding / 4),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
           ),
-        )
-      ],
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: kDefaultPadding / 4),
+              height: 7,
+              color: kPrimaryColor.withOpacity(0.2),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
